bc6ef8e11b8da140120b0584fd2cdc4bf951948b
kashirskii <kashirskiy.yegor@ya.ru>
2025-04-01 03:50:57 +0400

    ✨ feat: add Pagination component

---
+++ src/shared/ui/pagination.tsx
@@ -0,0 +1,247 @@
+import { cva } from "class-variance-authority";
+import { createContext, useContext, useState } from "react";
+import { NonNegative } from "../types";
+
+const paginationItem = cva(
+  "w-10 h-10 rounded-md flex items-center justify-center cursor-pointer",
+  {
+    variants: {
+      view: {
+        selected: "bg-black/6 text-black",
+        unselected: "bg-transparent text-black/50",
+      },
+    },
+  }
+);
+
+interface PaginationProps<T extends number> {
+  count: NonNegative<T>;
+  page?: NonNegative<T>;
+  defaultPage?: NonNegative<T>;
+  boundaryCount?: NonNegative<T>;
+  siblingCount?: NonNegative<T>;
+  onChange?: (page: number) => void;
+  children?: React.ReactNode;
+}
+
+interface PaginationContext<T extends number> {
+  page: number;
+  setPage: NonNullable<PaginationProps<T>["onChange"]>;
+  count: PaginationProps<T>["count"];
+  siblingCount: NonNullable<PaginationProps<T>["siblingCount"]>;
+  boundaryCount: NonNullable<PaginationProps<T>["boundaryCount"]>;
+}
+
+const PaginationContext = createContext<PaginationContext<number> | undefined>(
+  undefined
+);
+
+export const Pagination = <T extends number>({
+  page: controlledPage,
+  count,
+  defaultPage = 1 as NonNegative<T>,
+  siblingCount = 1 as NonNegative<T>,
+  boundaryCount = 1 as NonNegative<T>,
+  onChange,
+  children,
+  className,
+  ...props
+}: PaginationProps<T> &
+  Omit<
+    React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLDivElement>,
+      HTMLDivElement
+    >,
+    "onChange"
+  >) => {
+  const [internalPage, setInternalPage] = useState<number>(defaultPage);
+
+  const page = controlledPage ?? internalPage;
+  const setPage = onChange ?? setInternalPage;
+
+  return (
+    <PaginationContext.Provider
+      value={{ count, siblingCount, page, setPage, boundaryCount }}
+    >
+      <div className={`flex gap-1 ${className}`} {...props}>
+        {children}
+      </div>
+    </PaginationContext.Provider>
+  );
+};
+
+export const Items = ({
+  className,
+  ...props
+}: React.DetailedHTMLProps<
+  React.HTMLAttributes<HTMLUListElement>,
+  HTMLUListElement
+>) => {
+  const context = useContext(PaginationContext);
+  if (!context) return null;
+  const { page, count, siblingCount, boundaryCount } = context;
+
+  const totalBlocksCount = 3 + siblingCount * 2 + boundaryCount * 2;
+
+  if (count < totalBlocksCount) {
+    return Array.from({ length: count }, (_, index) => (
+      <PaginationItem key={index + 1} number={index + 1} />
+    ));
+  }
+
+  const shouldShowLeftEllipsis = () =>
+    page - siblingCount - (boundaryCount + 1) <= 1;
+
+  const shouldShowRightEllipsis = () =>
+    count - boundaryCount - (page + siblingCount) <= 1;
+
+  return (
+    <ul className={`flex gap-1 ${className}`} {...props}>
+      {[...Array(boundaryCount)].map((_, i) => (
+        <li>
+          <PaginationItem key={boundaryCount + i} number={i + 1} />
+        </li>
+      ))}
+
+      {shouldShowLeftEllipsis() ? (
+        <li>
+          <PaginationItem number={boundaryCount + 1} />
+        </li>
+      ) : (
+        <li>
+          <Ellipsis />
+        </li>
+      )}
+
+      {[...Array(siblingCount * 2 + 1)].map((_, i) => {
+        const pageNumber = Math.min(
+          Math.max(2 + boundaryCount, page - siblingCount) + i,
+          count - boundaryCount - 2 - (siblingCount * 2 - 1) + i
+        );
+        return (
+          <li>
+            <PaginationItem key={pageNumber} number={pageNumber} />
+          </li>
+        );
+      })}
+
+      {shouldShowRightEllipsis() ? (
+        <li>
+          <PaginationItem number={count - boundaryCount} />
+        </li>
+      ) : (
+        <li>
+          <Ellipsis />
+        </li>
+      )}
+
+      {[...Array(boundaryCount)].map((_, i) => (
+        <li>
+          <PaginationItem key={i} number={count - boundaryCount + i + 1} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const Ellipsis = () => (
+  <div className="w-10 h-10 flex items-center justify-center">...</div>
+);
+
+const PaginationItem = ({ number }: { number: number }) => {
+  const context = useContext(PaginationContext);
+  if (!context) return null;
+
+  const { page, setPage } = context;
+
+  return (
+    <button
+      onClick={() => setPage(number)}
+      className={paginationItem({
+        view: page == number ? "selected" : "unselected",
+      })}
+    >
+      {number}
+    </button>
+  );
+};
+
+const PreviousButton = ({
+  className,
+  ...props
+}: React.DetailedHTMLProps<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+>) => {
+  const context = useContext(PaginationContext);
+  if (!context) return null;
+  const { page, setPage } = context;
+
+  return (
+    <button
+      className={`w-fit flex border-black/10 border rounded-md items-center px-4 py-2 gap-3 cursor-pointer ${className}`}
+      onClick={() => setPage(page - 1)}
+      disabled={page === 1}
+      {...props}
+    >
+      <svg
+        width="14"
+        height="14"
+        viewBox="0 0 14 14"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M12.8332 6.99996H1.1665M1.1665 6.99996L6.99984 12.8333M1.1665 6.99996L6.99984 1.16663"
+          stroke="black"
+          strokeWidth="1.67"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+      Previous
+    </button>
+  );
+};
+
+const NextButton = ({
+  className,
+  ...props
+}: React.DetailedHTMLProps<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+>) => {
+  const context = useContext(PaginationContext);
+  if (!context) return null;
+  const { page, setPage, count } = context;
+
+  return (
+    <button
+      className={`w-fit flex border-black/10 border rounded-md items-center px-4 py-2 gap-3 cursor-pointer ${className}`}
+      onClick={() => setPage(page + 1)}
+      disabled={page === count}
+      {...props}
+    >
+      Next
+      <svg
+        width="14"
+        height="14"
+        viewBox="0 0 14 14"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M1.1665 6.99996H12.8332M12.8332 6.99996L6.99984 1.16663M12.8332 6.99996L6.99984 12.8333"
+          stroke="black"
+          strokeWidth="1.67"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </button>
+  );
+};
+
+Pagination.PreviousButton = PreviousButton;
+Pagination.NextButton = NextButton;
+Pagination.Items = Items;

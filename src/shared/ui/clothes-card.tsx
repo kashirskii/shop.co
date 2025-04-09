6cc0cb05e6a114dd095824595454889e16210a0b
kashirskii <kashirskiy.yegor@ya.ru>
2025-04-09 18:38:47 +0400

    ✨ feat: add ClothesCard component

---
+++ src/shared/ui/clothes-card.tsx
@@ -0,0 +1,54 @@
+export const ClothesCard = ({
+  className,
+  ...props
+}: React.ComponentProps<"div">) => (
+  <div className={`flex gap-4 ${className}`} {...props} />
+);
+
+export const Image = ({
+  className,
+  src,
+  ...props
+}: React.ComponentProps<"div"> & { src: string }) => {
+  return (
+    <div
+      className={`max-w-[300px] max-h-[300px] bg-[#F0EEED] rounded-2xl ${className}`}
+      {...props}
+    >
+      <img src={src} className="w-full h-full" />
+    </div>
+  );
+};
+
+Image.displayName = "ClothesCardImage";
+ClothesCard.Image = Image;
+
+export const Title = ({ className, ...props }: React.ComponentProps<"div">) => (
+  <div
+    className={`font-bold text-xl leading-none tracking-tight ${className}`}
+    {...props}
+  />
+);
+
+Title.displayName = "ClothesCardTitle";
+ClothesCard.Title = Title;
+
+export const Content = ({
+  className,
+  ...props
+}: React.ComponentProps<"div">) => (
+  <div className={`flex gap-2 ${className}`} {...props} />
+);
+
+Content.displayName = "ClothesCardContent";
+ClothesCard.Content = Content;
+
+export const Price = ({ className, ...props }: React.ComponentProps<"div">) => (
+  <div
+    className={`font-bold text-2xl tracking-tight ${className}`}
+    {...props}
+  />
+);
+
+Price.displayName = "ClothesCardPrice";
+ClothesCard.Price = Price;

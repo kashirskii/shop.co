6f94a9f9e50e2a580dd6f8cacb0b302dc3b42874
kashirskii <kashirskiy.yegor@ya.ru>
2025-04-10 18:24:52 +0400

    🔧 fix: convert array query to string for API

--- src/shared/api/requests/get/getClothes.ts
+++ src/shared/api/requests/get/getClothes.ts
@@ -10,8 +10,23 @@ export type GetClothesRequestConfig = RequestConfig<
   PageClothingResponseDto
 >;
 
-export const getClothes: GetClothesRequestConfig = ({ params, config } = {}) =>
-  instance.get("/clothes", {
-    params: { ...config?.params, ...params },
+export const getClothes: GetClothesRequestConfig = ({
+  params,
+  config,
+} = {}) => {
+  const { size, color, ...restParams } = params || {};
+
+  return instance.get("/clothes", {
+    params: {
+      ...config?.params,
+      ...(typeof size !== "undefined" && size.length > 0
+        ? { size: size.join(",") }
+        : null),
+      ...(typeof color !== "undefined" && color.length > 0
+        ? { color: color.join(",") }
+        : null),
+      ...restParams,
+    },
     ...config,
   });
+};
